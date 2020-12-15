aeb2fbec7d037872ee5fde9bf350e2a046bc4512
Arun Chinnapparaj <arunchinnapparaj@Chinna-MacBook.local>
2020-12-16 00:04:54 +0530

    added timer

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React,{useState} from "react";
+//import IdleTimer from 'react-idle-timer'
+import { useIdleTimer } from 'react-idle-timer'
 import BudgetBarChart from "../budgetBarChart/BudgetBarChart";
 import BudgetPieChart from "../budgetChart/BudgetPieChart";
 import ExpensePieChart from "../expensePieChart/ExpensePieChart";
@@ -6,8 +8,36 @@ import Expense from "../expense/Expense";
 
 import Navbar from "../navBar/NavBar";
 
-function Dashboard() {
-  return <div>
+
+const Dashboard = () => {
+  const [idle, setIdle] = useState(0);
+  const handleOnIdle = event => {
+    console.log('user is idle', event)
+    console.log('last active', getLastActiveTime())
+    setIdle(1)
+  }
+ 
+  const handleOnActive = event => {
+    console.log('user is active', event)
+    console.log('time remaining', getRemainingTime())
+    setIdle(1)
+  }
+ 
+  const handleOnAction = (e) => {
+    console.log('user did something', e)
+  }
+ 
+  const { getRemainingTime, getLastActiveTime } = useIdleTimer({
+    timeout: 10000,
+    onIdle: handleOnIdle,
+    onActive: handleOnActive,
+    onAction: handleOnAction,
+    debounce: 500
+  })
+
+  return (
+  <div>
+  <p>{ idle ? 'Logout':'Active'}</p>
       <div className="side-nav">
         <BudgetPieChart/>
         </div>
@@ -15,12 +45,12 @@ function Dashboard() {
         <BudgetBarChart/>
         </div>
         <div>
-          <ExpensePieChart/>
+        <ExpensePieChart/>
         </div>
         <div>
         <Expense/>
         </div>
-  </div>;
+  </div>)
 }
 
 export default Dashboard;
